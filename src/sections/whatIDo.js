22c26ce59df3736beb14cf891a50ff0470e53e8e
Amy Franz <amy.franz@gmail.com>
2021-04-10 16:25:37 +0100

    pwa

--- src/sections/whatIDo.js
+++ src/sections/whatIDo.js
@@ -1,6 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars} from '@fortawesome/free-solid-svg-icons'
-
 function WhatIDo() {
     return (
       <div style={{"width": "95vw"}}>
